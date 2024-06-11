4d8dd3f17399f149ec126e38173b44dd86de9ad9
OliverLysa <oliverlysaght@icloud.com>
2024-06-11 11:00:48 -0600

    textiles

--- textiles/textiles_trade.R
+++ textiles/textiles_trade.R
@@ -35,6 +35,8 @@ if (any(installed_packages == FALSE)) {
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
 
+Sys.setenv('COMTRADE_PRIMARY' = '4bb0e2c57f5d4dfc8314f732ae82e1e3')
+
 # *******************************************************************************
 # Import functions, options and connections 
 # *******************************************************************************
@@ -87,80 +89,102 @@ trade_terms <-
 #
 
 # UKTradeData - goes back to 2000
-trade_terms_CN8 <-trade_terms %>%
+trade_terms_CN8 <- trade_terms %>%
   # Isolate list of CN8 codes from classification table, column 'CN8', extract unique codes and unlist
-  select(CN8) %>%
+  select(HS2) %>%
+  unique() %>%
   unlist()
 
-# If a subset of those codes are sought, these can be selected by index position
-# Using this as in some cases, a memory issue arise with full list
-# trade_terms_CN8 <- trade_terms_CN8[1106:1242]
+# # If a subset of those codes are sought, these can be selected by index position
+# # Using this as in some cases, a memory issue arise with full list
+# # trade_terms_CN8 <- trade_terms_CN8[1106:1242]
+# 
+# # Create a for loop that goes through the trade terms, extracts the data using the extractor function (in function script) based on the uktrade wrapper
+# # and prints the results to a list of dataframes
+# res <- list()
+# for (i in seq_along(trade_terms_CN8)) {
+#   res[[i]] <- extractor(trade_terms_CN8[i])
+#   
+#   print(i)
+#   
+# }
+# 
+# # Bind the list of returned dataframes to a single dataframe
+# bind2 <- 
+#   dplyr::bind_rows(res)
+# 
+# # If you have not used the in-built lookup codes in the uktrade R package, describe the flow-types for subsequent aggregation
+# bind2 <- bind2 %>%
+#   mutate(FlowTypeId = gsub(1, 'EU Imports', FlowTypeId),
+#          FlowTypeId = gsub(2, 'EU Exports', FlowTypeId),
+#          FlowTypeId = gsub(3, 'Non-EU Imports', FlowTypeId),
+#          FlowTypeId = gsub(4, 'Non-EU Exports', FlowTypeId)) %>%
+#   rename(FlowTypeDescription = FlowTypeId)
+# 
+# # bind3 <- bind3 %>%
+# #   mutate(FlowTypeDescription = gsub('EU Imports', 'Imports', FlowTypeDescription),
+# #          FlowTypeDescription = gsub('EU Exports', 'Exports', FlowTypeDescription),
+# #          FlowTypeDescription = gsub('`Non-EU Imports`', 'Imports', FlowTypeDescription),
+# #          FlowTypeDescription = gsub('Non-EU Exports', 'Exports', FlowTypeDescription),
+# #          FlowTypeDescription = gsub('Non-Exports', 'Exports', FlowTypeDescription),
+# #          FlowTypeDescription = gsub('Non-Imports', 'Imports', FlowTypeDescription))
+# 
+# # Remove the month identifier in the month ID column to be able to group by year
+# # This can be removed for more time-granular data e.g. by month or quarter
+# bind$MonthId <- 
+#   substr(bind$MonthId, 1, 4)
+# 
+# # Summarise results in value, mass and unit terms grouped by year, flow type and trade code as well as broad trade direction
+# summary_trade_no_country <- bind %>%
+#   group_by(MonthId, 
+#            FlowTypeDescription, 
+#            CommodityId) %>%
+#   summarise(sum(Value), 
+#             sum(NetMass), 
+#             sum(SuppUnit)) %>%
+#   rename(Year = MonthId) %>%
+#   # Pivot results longer
+#   pivot_longer(-c(Year, 
+#                   FlowTypeDescription, 
+#                   CommodityId),
+#                names_to = "Variable",
+#                values_to = 'Value') %>%
+#   # Convert trade code to character
+#   mutate_at(c(3), as.character) %>%
+#   left_join(# Join the correspondence codes and the trade data
+#     trade_terms,
+#     by =join_by("CommodityId" == "CN8")) %>%
+#   rename(CN = CommodityId)
+# 
+# # Export locally
+# write_xlsx(summary_trade_no_country,
+#           "summary_trade_no_country_50_only.xlsx")
+# 
+# # Export to database
+# DBI::dbWriteTable(con, 
+#                   "textiles_trade", 
+#                   summary_trade_no_country)
 
-# Create a for loop that goes through the trade terms, extracts the data using the extractor function (in function script) based on the uktrade wrapper
-# and prints the results to a list of dataframes
-res <- list()
-for (i in seq_along(trade_terms_CN8)) {
-  res[[i]] <- extractor(trade_terms_CN8[i])
-  
-  print(i)
-  
-}
+# Hitting API without package - confirms there are some issues with the package
+# https://api.uktradeinfo.com/OTS?$filter=(MonthId ge 202301 and MonthId le 202312) and ((CommodityId ge 54 and CommodityId le 54))
+# https://api.uktradeinfo.com/OTS?$filter=(MonthId%20ge%20202301%20and%20MonthId%20le%20202312)%20and%20((CommodityId%20ge%2054%20and%20CommodityId%20le%2054))
+raw = GET(paste('https://api.uktradeinfo.com/OTS?$filter=(MonthId%20ge%20202301%20and%20MonthId%20le%20202312)%20and%20((CommodityId%20ge%2054%20and%20CommodityId%20le%2054))'))
 
-# Bind the list of returned dataframes to a single dataframe
-bind <- 
-  dplyr::bind_rows(res)
+#convert to a character string
+r2 <- rawToChar(raw$content)  
 
-# If you have not used the in-built lookup codes in the uktrade R package, describe the flow-types for subsequent aggregation
-bind <- bind %>%
-  mutate(FlowTypeId = gsub(1, 'EU Imports', FlowTypeId),
-         FlowTypeId = gsub(2, 'EU Exports', FlowTypeId),
-         FlowTypeId = gsub(3, 'Non-EU Imports', FlowTypeId),
-         FlowTypeId = gsub(4, 'Non-EU Exports', FlowTypeId)) %>%
-  rename(FlowTypeDescription = FlowTypeId)
+#check the class is character
+class(r2)    
 
-bind3 <- bind3 %>%
-  mutate(FlowTypeDescription = gsub('EU Imports', 'Imports', FlowTypeDescription),
-         FlowTypeDescription = gsub('EU Exports', 'Exports', FlowTypeDescription),
-         FlowTypeDescription = gsub('`Non-EU Imports`', 'Imports', FlowTypeDescription),
-         FlowTypeDescription = gsub('Non-EU Exports', 'Exports', FlowTypeDescription),
-         FlowTypeDescription = gsub('Non-Exports', 'Exports', FlowTypeDescription),
-         FlowTypeDescription = gsub('Non-Imports', 'Imports', FlowTypeDescription))
+# now extract JSON from string object
+r3 <- fromJSON(r2)
 
-# Remove the month identifier in the month ID column to be able to group by year
-# This can be removed for more time-granular data e.g. by month or quarter
-bind$MonthId <- 
-  substr(bind2$MonthId, 1, 4)
+r4 <- r3$value
 
-# Summarise results in value, mass and unit terms grouped by year, flow type and trade code as well as broad trade direction
-summary_trade_no_country <- bind %>%
-  group_by(MonthId, 
-           FlowTypeDescription, 
-           CommodityId) %>%
-  summarise(sum(Value), 
-            sum(NetMass), 
-            sum(SuppUnit)) %>%
-  rename(Year = MonthId) %>%
-  # Pivot results longer
-  pivot_longer(-c(Year, 
-                  FlowTypeDescription, 
-                  CommodityId),
-               names_to = "Variable",
-               values_to = 'Value') %>%
-  # Convert trade code to character
-  mutate_at(c(3), as.character) %>%
-  left_join(# Join the correspondence codes and the trade data
-    trade_terms,
-    by =join_by("CommodityId" == "CN8")) %>%
-  rename(CN = CommodityId)
+r5<- bind_rows(r4)
 
-# Export locally
-write_xlsx(summary_trade_no_country,
-          "summary_trade_no_country.xlsx")
-
-# Export to database
-DBI::dbWriteTable(con, 
-                  "textiles_trade", 
-                  summary_trade_no_country)
+write_xlsx(r5,
+           "raw_api_output.xlsx")
 
 # Comtrade
 
@@ -168,7 +192,10 @@ DBI::dbWriteTable(con,
 trade_terms_HS6 <-trade_terms %>%
   # Isolate list of CN8 codes from classification table, column 'CN8', extract unique codes and unlist
   select(HS6) %>%
+  unique() %>%
   unlist()
+
+trade_terms_HS6 <- trade_terms_HS6[1:499]
 
 # Function to use the comtrade R package to extract trade data from the Comtrade API
 comtrade_extractor <- function(x) {
@@ -203,3 +230,8 @@ for (i in seq_along(trade_terms_HS6)) {
 # Bind the list of returned dataframes to a single dataframe
 bind_com <-
   dplyr::bind_rows(res)
+
+# Export locally
+write_xlsx(bind_com,
+           "bind_com.xlsx")
+
