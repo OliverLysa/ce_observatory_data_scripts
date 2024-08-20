07d35009ff82b2c4b8388f19d66dd98529aba85e
OliverLysa <oliverlysaght@icloud.com>
2024-08-20 11:45:57 -0600

    tax:

--- plastics/plasticpackaging_tx_HMRC.R
+++ plastics/plasticpackaging_tx_HMRC.R
@@ -1,6 +1,6 @@
 ##### **********************
 # Author: Matt and Oliver
-# Data update frequency: Quarterly
+# Data update frequency: Annual
 
 # *******************************************************************************
 # Packages
@@ -51,15 +51,43 @@ options(scipen = 999)
 # *******************************************************************************
 #
 
+# download.file(
+#   "https://assets.publishing.service.gov.uk/media/64d4b18f5cac650014c2dd2a/PPT_statistics_ODS.xlsx",
+#   "./raw_data/HMRC_tax_revenue.xlsx"
+# )
+
 download.file(
-  "https://assets.publishing.service.gov.uk/media/64d4b18f5cac650014c2dd2a/PPT_statistics_ODS.xlsx",
-  "./raw_data/HMRC_tax_revenue.xlsx"
+  "https://assets.publishing.service.gov.uk/media/66b4bd2749b9c0597fdb0cd7/PPT_statistics_ODS_year_2.ods",
+  "./raw_data/HMRC_tax_revenue_update.ods"
 )
+
+# PPT_statistics <-
+#   # Read in file
+#   read_excel("./raw_data/HMRC_tax_revenue.xlsx",
+#              sheet = "Table 1 & 2") %>%
+#   # Remove nas
+#   na.omit() %>%
+#   # Make row 1 the column names
+#   row_to_names(1) %>%
+#   # Clean those column names
+#   clean_names() %>%
+#   # Remove row with string detected in column
+#   filter(! str_detect(table_1, 'Table 2')) %>%
+#   pivot_longer(-c(table_1),
+#                names_to = 'variable') %>%
+#   mutate(frequency = ifelse(grepl("2022 to 2023", table_1),'annual','quarterly')) %>%
+#   mutate(unit = case_when(str_detect(variable, "revenue") ~ "Monetary",
+#                           str_detect(variable, "tonnage") ~ "Tonnes",
+#                           str_detect(variable, "number") ~ "Number")) %>%
+#   filter(! str_detect(value, '[x]')) %>%
+#   mutate(table_1 = gsub("\\[provisional\\]", "", table_1))
+
+### Updated version
 
 PPT_statistics <-
   # Read in file
-  read_excel("./raw_data/HMRC_tax_revenue.xlsx",
-             sheet = "Table 1 & 2")%>%
+  read_ods("./raw_data/HMRC_tax_revenue_update.ods",
+             sheet = "Table_1_&_2") %>%
   # Remove nas
   na.omit() %>%
   # Make row 1 the column names
@@ -70,23 +98,17 @@ PPT_statistics <-
   filter(! str_detect(table_1, 'Table 2')) %>%
   pivot_longer(-c(table_1),
                names_to = 'variable') %>%
-  mutate(frequency = ifelse(grepl("2022 to 2023", table_1),'annual','quarterly')) %>%
+  mutate(frequency = ifelse(grepl("to", table_1),'annual','quarterly')) %>%
   mutate(unit = case_when(str_detect(variable, "revenue") ~ "Monetary",
                           str_detect(variable, "tonnage") ~ "Tonnes",
                           str_detect(variable, "number") ~ "Number")) %>%
-  filter(! str_detect(value, '[x]'))
-
-# connecting to database
-con <- dbConnect(RPostgres::Postgres(),
-                 dbname = 'postgres',
-                 host = 'aws-0-eu-west-2.pooler.supabase.com',
-                 port = 5432,
-                 user = 'postgres.qowfjhidbxhtdgvknybu',
-                 password = rstudioapi::askForPassword("Database password"))
+  filter(! str_detect(value, '[x]')) %>%
+  mutate(table_1 = gsub("\\[revised\\]", "", table_1)) %>%
+  mutate(table_1 = gsub("\\[provisional\\]", "", table_1)) %>%
+  na.omit() %>%
+  rename(period = 1)
 
 # Export to database
 DBI::dbWriteTable(con, "pptxeffects",
                   PPT_statistics,
                   overwrite = TRUE)
-
-write.csv(PPT_statistics, "pptxeffects.csv")
