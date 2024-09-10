9bde65639013bae0968e14167ebfe9ffdac2f724
OliverLysa <oliverlysaght@icloud.com>
2024-09-10 11:48:16 -0600

    plastics

--- plastics/KBT_litter.R
+++ plastics/KBT_litter.R
@@ -12,14 +12,17 @@ options(scipen=999)
 dataset <- 
   read_excel("./raw_data/KBT_Litter_Composition_Survey.xlsx", sheet = 1) 
 
-datasettrimmed <- dataset[c(1:3361), c(1, 23:68)] %>% 
-  pivot_longer(-c(Identifier), names_to = c("Litter_Type")) %>% 
-  filter(value != 0)
-
-datasettrimmed$Litter_Type <- gsub(".*- ","", datasettrimmed$Litter_Type)
-
-Litter_Types <- datasettrimmed %>% 
-  group_by(Litter_Type) %>% 
+datasettrimmed <- dataset[c(1:3361), c(10, 23:68)] %>% 
+  pivot_longer(-c(Region), names_to = c("Litter_Type")) %>% 
+  filter(value != 0) %>%
+  mutate(Litter_Type = gsub("Litter Type Counts - ", "", Litter_Type)) %>% 
+  group_by(Litter_Type, Region) %>% 
   summarise(Value = sum(value)) %>% 
-  mutate(freq = Value / sum(Value)*100)
+  mutate(freq = Value / sum(Value)*100) %>%
+  mutate(across(is.numeric, round, digits=1)) %>%
+  select(-Value)
 
+DBI::dbWriteTable(con, 
+                  "litter_proportions",
+                  datasettrimmed,
+                  overwrite = TRUE)
