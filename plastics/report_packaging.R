ef6416112ac6f2088f0d277ee3a12e1b60dc0e30
OliverLysa <oliverlysaght@icloud.com>
2024-11-07 13:22:29 -0700

    plastics

--- plastics/report_packaging.R
+++ plastics/report_packaging.R
@@ -1,27 +1,5 @@
 # Report packaging data
 
-# This data was extracted from the Report Packaging Data (RPD) service on the 14th October 2024. RPD is the digital service for the extended producer responsibility (EPR) for packaging. Find out more about EPR
-
-# This data report provides the UK aggregated packaging data, broken down by material and packaging type in tonnes, that has been reported for 2023 under the Packaging Waste (Data Reporting) Regulations 2023.
-# This data was extracted from the Report Packaging Data (RPD) service on the 14th October 2024. RPD is the digital service for the extended producer responsibility (EPR) for packaging. Find out more about EPR.
-# 
-# These figures include 2023 packaging data submitted by UK large producers; the figures do not include:
-#   
-#  Data from large producers who are required to report their data but have not
-# 
-# Data from large producers who reported their 2023 data after the 14th October 2024
-# 
-# This includes some partial data submitted for 1 March to 31 December 2023. For Welsh producers, some data submitted covered 17 July to 31 December 2023.
-# 
-# This data has no uplift applied where producers have submitted partial data.
-# 
-# The data is subject to change due to late submissions, resubmissions and new submissions.
-# 
-# This is the first year of reporting under the Data reporting regulations. The regulators have a duty to monitor the accuracy of pEPR data submitted into RPD; these checks are ongoing. Regulators are also working with producers to identify and correct possible data errors. As a result, the data that makes up these total figures may include reporting issues that have not yet been addressed.
-# 
-# 2023 data is not impacted by The Packaging Waste (Data Reporting) (Amendment) Regulations 2024 as passed in each nation within the UK. For example, the requirement to report drinks containers supplied in Scotland was excluded for the calendar year 2023. The requirement to report drinks containers supplied in Scotland was introduced from 2024 onwards.
-# 
-
 # *******************************************************************************
 # Require packages
 # *******************************************************************************
