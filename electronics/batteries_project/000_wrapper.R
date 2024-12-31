dc4dc3f4508af4adfb8d4d52162393474fef1df9
OliverLysa <oliverlysaght@icloud.com>
2024-12-30 22:24:20 -0700

    batteries

---
+++ electronics/batteries_project/000_wrapper.R
@@ -0,0 +1,23 @@
+# Files are organised below to be ran sequentially
+# These can be ran from the command line using the following
+# Rscript wrapper.R
+
+source(
+  "/Users/oliverlysaght/Desktop/ce_observatory_data_scripts/electronics/batteries_project/001_international_trade.R"
+)
+source(
+  "/Users/oliverlysaght/Desktop/ce_observatory_data_scripts/electronics/batteries_project/002_domestic_production.R"
+)
+source(
+  "/Users/oliverlysaght/Desktop/ce_observatory_data_scripts/electronics/batteries_project/003_total_inflows.R"
+)
+source(
+  "/Users/oliverlysaght/Desktop/ce_observatory_data_scripts/electronics/batteries_project/004_mass_conversion.R"
+)
+source(
+  "/Users/oliverlysaght/Desktop/ce_observatory_data_scripts/electronics/batteries_project/005_stock_outflow.R"
+)
+source(
+  "/Users/oliverlysaght/Desktop/ce_observatory_data_scripts/electronics/batteries_project/006_outflow_routing.R"
+)
+
