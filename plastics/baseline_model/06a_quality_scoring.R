af2fe2bf45d9bcbf4b723ff9441cd3c9d9cc44b7
OliverLysa <70466870+OliverLysa@users.noreply.github.com>
2025-02-17 21:34:49 -0700

    Add files via upload

--- plastics/baseline_model/06a_quality_scoring.R
+++ plastics/baseline_model/06a_quality_scoring.R
@@ -8,7 +8,7 @@
 
 ## Import the sankey data with sources attached
 plastic_packaging_sankey_flows <-
-  read_csv("sankey_all.csv") %>%
+  read_csv("./cleaned_data/sankey_all.csv") %>%
   # Add the region to the sankey for the geographical correlation
   mutate(region = "UK") %>%
   separate_wider_delim(data_source_id,
@@ -22,6 +22,7 @@ metadata <-
   read_excel("./raw_data/data_sources.xlsx") %>%
   clean_names()
 
+# Clean up column names for joining
 for ( col in 1:ncol(metadata)){
   colnames(metadata)[col] <-  sub("percent_percent._*", "", colnames(metadata)[col])
 }
@@ -46,7 +47,7 @@ for ( col in 1:ncol(metadata)){
 # Join the metadata catalogue to the sankey columns
 plastic_packaging_sankey_flows_geographical_source_1 <-
   left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id1" = "name_identifier")) %>%
-  select(1:11,26) %>%
+  select(1:11,29) %>%
   # See if a match on region
   mutate(patterns = map_chr(strsplit(region, ", "),paste,collapse="|"),
          match = str_detect(spatial_coverage_and_detail_geographical_coverage, patterns)) %>%
@@ -57,7 +58,7 @@ plastic_packaging_sankey_flows_geographical_source_1 <-
 
 plastic_packaging_sankey_flows_geographical_source_2 <-
   left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id2" = "name_identifier")) %>%
-  select(1:11,26) %>%
+  select(1:11,29) %>%
   # See if a match on region
   mutate(patterns = map_chr(strsplit(region, ", "),paste,collapse="|"),
        match = str_detect(spatial_coverage_and_detail_geographical_coverage, patterns)) %>%
@@ -68,7 +69,7 @@ plastic_packaging_sankey_flows_geographical_source_2 <-
 
 plastic_packaging_sankey_flows_geographical_source_3 <-
   left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id3" = "name_identifier")) %>%
-  select(1:11,26) %>%
+  select(1:11,29) %>%
   # See if a match on region
   mutate(patterns = map_chr(strsplit(region, ", "),paste,collapse="|"),
          match = str_detect(spatial_coverage_and_detail_geographical_coverage, patterns)) %>%
@@ -105,7 +106,7 @@ plastic_packaging_sankey_flows_geographical <-
 
 # Create a lookup table of the years each source covers
 timeliness_metadata <- metadata %>%
-  select(1,18) %>%
+  select(1,21) %>%
   separate_wider_delim(timeliness_period_covered,
                        delim = ";",
                        too_few = "align_start",
@@ -157,7 +158,7 @@ plastic_packaging_sankey_flows_temporal_source_3 <- plastic_packaging_sankey_flo
                                                                NA))))) %>%
   select(11,14)
 
-# Take an average for the score across the sources
+# Take an average across the sources for the score
 plastic_packaging_sankey_flows_temporal <- 
   left_join(plastic_packaging_sankey_flows_temporal_source_1, plastic_packaging_sankey_flows_temporal_source_2, by='row_id') %>%
   left_join(., plastic_packaging_sankey_flows_temporal_source_3, by='row_id') %>%
@@ -181,31 +182,95 @@ plastic_packaging_sankey_flows_temporal <-
 # }]
 # 
 
-# Linked variables in the metadata catalogue
-# Specific to or just covers - is it specifically named in that column? Otherwise, is it link to the named process (separate lookup)
-# Product - packaging
-# Material - Polymer
-# Process - The process e.g. landfilling 
+# Linked variables in the metadata catalogue - sub-scoring scheme
+# Product(s) covered - Plastic Packaging
+# Product relevance column - options
+# 1. Category in the data specific to plastic packaging used
+# 2. Category in the data specific to plastic or packaging used, but not specifically plastic packaging
+# 3. Category in the data not specific to plastic or packaging used
+# Technology - The process
 
 # Completeness ------------------------------------------------------------
 
 # The extent to which all relevant mass flows are accounted for.	[{
 #   score:1, definition:"Value includes all relevant processes/flows.",
 # } , {
-#   score:2, definition:"Value includes quantitatively main processes/flows.",
+#   score:2, definition:"Value includes main processes/flows.",
 # } , {
 #   score:3, definition:"Value includes partial important processes/flows, certainty of data gaps.",
 # } , {
-#   score:4, definition:"Only fragmented data available; important processes/mass flows are missing..",
+#   score:4, definition:"Only fragmented data available; important processes/mass flows are missing",
 # }]
 
-# Add completeness to data sources - an aggregate across product, material, technology/process
+# Add completeness to data sources - an average across product, material, technology/process and then across the data sources
+# Provide an estimated % of the flow, material and product covered
+plastic_packaging_sankey_flows_completeness_source_1 <-
+  left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id1" = "name_identifier")) %>%
+  select(1:6,11,24,26) %>%
+  mutate(completeness_material_product = ifelse(
+    input_requirement_mapping_material_and_product_completeness <= 100 & input_requirement_mapping_material_and_product_completeness <= 90, 1,
+                                                 ifelse(input_requirement_mapping_material_and_product_completeness <90 & input_requirement_mapping_material_and_product_completeness >= 75, 2,
+                                                        ifelse(input_requirement_mapping_material_and_product_completeness <75 & input_requirement_mapping_material_and_product_completeness >= 50, 3,
+                                                               ifelse(input_requirement_mapping_material_and_product_completeness <50 & input_requirement_mapping_material_and_product_completeness >= 0, 4,
+                                                               NA))))) %>%
+  mutate(completeness_technology = ifelse(
+    input_requirement_mapping_material_and_product_completeness <= 100 & input_requirement_mapping_material_and_product_completeness <= 90, 1,
+    ifelse(input_requirement_mapping_material_and_product_completeness <90 & input_requirement_mapping_material_and_product_completeness >= 75, 2,
+           ifelse(input_requirement_mapping_material_and_product_completeness <75 & input_requirement_mapping_material_and_product_completeness >= 50, 3,
+                  ifelse(input_requirement_mapping_material_and_product_completeness <50 & input_requirement_mapping_material_and_product_completeness >= 0, 4,
+                         NA))))) %>%
+  mutate(completeness_score_1 = (completeness_material_product + completeness_technology)/2) %>%
+  select(1:5,7,12)
+
+plastic_packaging_sankey_flows_completeness_source_2 <-
+  left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id2" = "name_identifier")) %>%
+  select(7,11,24,26) %>%
+  mutate(completeness_material_product = ifelse(
+    input_requirement_mapping_material_and_product_completeness <= 100 & input_requirement_mapping_material_and_product_completeness <= 90, 1,
+    ifelse(input_requirement_mapping_material_and_product_completeness <90 & input_requirement_mapping_material_and_product_completeness >= 75, 2,
+           ifelse(input_requirement_mapping_material_and_product_completeness <75 & input_requirement_mapping_material_and_product_completeness >= 50, 3,
+                  ifelse(input_requirement_mapping_material_and_product_completeness <50 & input_requirement_mapping_material_and_product_completeness >= 0, 4,
+                         NA))))) %>%
+  mutate(completeness_technology = ifelse(
+    input_requirement_mapping_material_and_product_completeness <= 100 & input_requirement_mapping_material_and_product_completeness <= 90, 1,
+    ifelse(input_requirement_mapping_material_and_product_completeness <90 & input_requirement_mapping_material_and_product_completeness >= 75, 2,
+           ifelse(input_requirement_mapping_material_and_product_completeness <75 & input_requirement_mapping_material_and_product_completeness >= 50, 3,
+                  ifelse(input_requirement_mapping_material_and_product_completeness <50 & input_requirement_mapping_material_and_product_completeness >= 0, 4,
+                         NA))))) %>%
+  mutate(completeness_score_2 = (completeness_material_product + completeness_technology)/2) %>%
+  select(1:5,7,12)
+
+plastic_packaging_sankey_flows_completeness_source_3 <-
+  left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id3" = "name_identifier")) %>%
+  select(8,11,24,26) %>%
+  mutate(completeness_material_product = ifelse(
+    input_requirement_mapping_material_and_product_completeness <= 100 & input_requirement_mapping_material_and_product_completeness <= 90, 1,
+    ifelse(input_requirement_mapping_material_and_product_completeness <90 & input_requirement_mapping_material_and_product_completeness >= 75, 2,
+           ifelse(input_requirement_mapping_material_and_product_completeness <75 & input_requirement_mapping_material_and_product_completeness >= 50, 3,
+                  ifelse(input_requirement_mapping_material_and_product_completeness <50 & input_requirement_mapping_material_and_product_completeness >= 0, 4,
+                         NA))))) %>%
+  mutate(completeness_technology = ifelse(
+    input_requirement_mapping_material_and_product_completeness <= 100 & input_requirement_mapping_material_and_product_completeness <= 90, 1,
+    ifelse(input_requirement_mapping_material_and_product_completeness <90 & input_requirement_mapping_material_and_product_completeness >= 75, 2,
+           ifelse(input_requirement_mapping_material_and_product_completeness <75 & input_requirement_mapping_material_and_product_completeness >= 50, 3,
+                  ifelse(input_requirement_mapping_material_and_product_completeness <50 & input_requirement_mapping_material_and_product_completeness >= 0, 4,
+                         NA))))) %>%
+  mutate(completeness_score_3 = (completeness_material_product + completeness_technology)/2) %>%
+  select(1:5,7,12)
+
+plastic_packaging_sankey_flows_completeness <- 
+  left_join(plastic_packaging_sankey_flows_completeness_source_1, plastic_packaging_sankey_flows_completeness_source_2, by='row_id') %>%
+  left_join(., plastic_packaging_sankey_flows_completeness_source_3, by='row_id') %>%
+  rowwise() %>% 
+  mutate(completeness_score = mean(c_across(c(completeness_score_1, 
+                                              completeness_score_2,
+                                              completeness_score_3)), na.rm = TRUE)) %>%
+  select(6,10) %>%
+  mutate(across(c('completeness_score'), round, 0))
 
 # Reliability -------------------------------------------------------------
-# Also a function of any estimation required to counteract a lack of completeness 
-# Reliability	The source of the data, including documentation of how the data was generated, verification methods, and whether the data has been peer -
-#   reviewed.	[{
-#     score:1, definition:"Well-documented, consistent, and peer-reviewed data.",
+# Reliability	The source of the data, including documentation of how the data was generated, verification methods, and whether the data has been peer -reviewed.	[{
+#     score:1, definition:"Methodology documented, consistent, and peer-reviewed data.",
 #   } , {
 #     score:2, definition:"Methodology described but not fully transparent, no verification.",
 #   } , {
@@ -215,15 +280,71 @@ plastic_packaging_sankey_flows_temporal <-
 #   }]
 
 ## Variables linked to 
-# Methodology documented 
-# Qualitative communication of uncertainty
-# Quantitative communication of uncertainty (if applicable)
-# Stated quality assurance process
-# Reproducibility via code
+# Communication of uncertainty - 1) Comprehensive and quantified, 2) Comprehensive; 3) Some; 4) None
+# Stated quality assurance process - 1) Official accredited statistics or equivalent e.g. peer review; 2) Informal peer review; 3) Stated administrative checking process; 4) 
+
+plastic_packaging_sankey_flows_reliability_source_1 <-
+  left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id1" = "name_identifier")) %>%
+  select(1:6,11,34,35) %>%
+  mutate(reliability_uncertainty = case_when(str_detect(quality_communication_of_methodology_and_data_quality, "Comprehensive and quantified") ~ 1,
+                                              str_detect(quality_communication_of_methodology_and_data_quality, "Comprehensive") ~ 2,
+         str_detect(quality_communication_of_methodology_and_data_quality, "Some") ~ 3,
+         str_detect(quality_communication_of_methodology_and_data_quality, "None") ~ 4)) %>%
+  mutate(reliability_qa = case_when(str_detect(quality_stated_and_accredited_quality_assurance, "Official accredited statistics") ~ 1,
+                                             str_detect(quality_stated_and_accredited_quality_assurance, "Informal peer review") ~ 2,
+                                             str_detect(quality_stated_and_accredited_quality_assurance, "Official statistics") ~ 2,
+                                             str_detect(quality_stated_and_accredited_quality_assurance, "Administrative checking") ~ 3,
+                                             str_detect(quality_stated_and_accredited_quality_assurance, "None") ~ 4)) %>%
+  mutate(reliability_score_1 = (reliability_uncertainty + reliability_qa)/2) %>%
+  select(1:5,7,12)
+
+plastic_packaging_sankey_flows_reliability_source_2 <-
+  left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id2" = "name_identifier")) %>%
+  select(7,11,34,35) %>%
+  mutate(reliability_uncertainty = case_when(str_detect(quality_communication_of_methodology_and_data_quality, "Comprehensive and quantified") ~ 1,
+                                             str_detect(quality_communication_of_methodology_and_data_quality, "Comprehensive") ~ 2,
+                                             str_detect(quality_communication_of_methodology_and_data_quality, "Some") ~ 3,
+                                             str_detect(quality_communication_of_methodology_and_data_quality, "None") ~ 4)) %>%
+  mutate(reliability_qa = case_when(str_detect(quality_stated_and_accredited_quality_assurance, "Official accredited statistics") ~ 1,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "Informal peer review") ~ 2,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "Official statistics") ~ 2,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "Administrative checking") ~ 3,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "None") ~ 4)) %>%
+  mutate(reliability_score_2 = (reliability_uncertainty + reliability_qa)/2) %>%
+  select(2,7)
+
+plastic_packaging_sankey_flows_reliability_source_3 <-
+  left_join(plastic_packaging_sankey_flows, metadata, c("data_source_id3" = "name_identifier")) %>%
+  select(8,11,34,35) %>%
+  mutate(reliability_uncertainty = case_when(str_detect(quality_communication_of_methodology_and_data_quality, "Comprehensive and quantified") ~ 1,
+                                             str_detect(quality_communication_of_methodology_and_data_quality, "Comprehensive") ~ 2,
+                                             str_detect(quality_communication_of_methodology_and_data_quality, "Some") ~ 3,
+                                             str_detect(quality_communication_of_methodology_and_data_quality, "None") ~ 4)) %>%
+  mutate(reliability_qa = case_when(str_detect(quality_stated_and_accredited_quality_assurance, "Official accredited statistics") ~ 1,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "Informal peer review") ~ 2,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "Official statistics") ~ 2,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "Administrative checking") ~ 3,
+                                    str_detect(quality_stated_and_accredited_quality_assurance, "None") ~ 4)) %>%
+  mutate(reliability_score_3 = (reliability_uncertainty + reliability_qa)/2) %>%
+  select(2,7)
+
+plastic_packaging_sankey_flows_reliability <- 
+  left_join(plastic_packaging_sankey_flows_reliability_source_1, plastic_packaging_sankey_flows_reliability_source_2, by='row_id') %>%
+  left_join(., plastic_packaging_sankey_flows_reliability_source_3, by='row_id') %>%
+  rowwise() %>% 
+  mutate(reliability_score = mean(c_across(c(reliability_score_1, 
+                                             reliability_score_2,
+                                             reliability_score_3)), na.rm = TRUE)) %>%
+  select(6,10) %>%
+  mutate(across(c('reliability_score'), round, 0))
+
+# Combine the scores into an array -------------------------------------------------------------
 
 # Join the tables together
 plastic_packaging_sankey_flows_quality_scores <- 
-  left_join(plastic_packaging_sankey_flows_geographical, plastic_packaging_sankey_flows_temporal, by='row_id')
+  left_join(plastic_packaging_sankey_flows_geographical, plastic_packaging_sankey_flows_temporal, by='row_id') %>%
+  left_join(., plastic_packaging_sankey_flows_reliability, by='row_id') %>%
+  left_join(., plastic_packaging_sankey_flows_completeness, by='row_id')
 
 write_csv(plastic_packaging_sankey_flows_quality_scores, 
           "sankey_all_quality_scores.csv")
