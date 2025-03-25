fed4eb8ca1e34b4e782276122812b44ff3b8d1d5
OliverLysa <70466870+OliverLysa@users.noreply.github.com>
2025-03-24 23:23:16 -0600

    Update functions.R

--- functions.R
+++ functions.R
@@ -61,60 +61,6 @@ extractor <- function(x) {
   return(trade_results)
 }
 
-# Use the comtrade API to extract historic trade data
-# Define get comtrade function
-get.Comtrade <- function(url="http://comtrade.un.org/api/get?"
-                         ,maxrec=50000
-                         ,type="C"
-                         ,freq="A"
-                         ,px="HS"
-                         ,ps="now"
-                         ,r
-                         ,p
-                         ,rg="all"
-                         ,cc="TOTAL"
-                         ,fmt="json"
-)
-{
-  string<- paste(url
-                 ,"max=",maxrec,"&" #maximum no. of records returned
-                 ,"type=",type,"&" #type of trade (c=commodities)
-                 ,"freq=",freq,"&" #frequency
-                 ,"px=",px,"&" #classification
-                 ,"ps=",ps,"&" #time period
-                 ,"r=",r,"&" #reporting area
-                 ,"p=",p,"&" #partner country
-                 ,"rg=",rg,"&" #trade flow
-                 ,"cc=",cc,"&" #classification code
-                 ,"fmt=",fmt        #Format
-                 ,sep = ""
-  )
-  
-  if(fmt == "csv") {
-    raw.data<- read.csv(string,header=TRUE)
-    return(list(validation=NULL, data=raw.data))
-  } else {
-    if(fmt == "json" ) {
-      raw.data<- fromJSON(file=string)
-      data<- raw.data$dataset
-      validation<- unlist(raw.data$validation, recursive=TRUE)
-      ndata<- NULL
-      if(length(data)> 0) {
-        var.names<- names(data[[1]])
-        data<- as.data.frame(t( sapply(data,rbind)))
-        ndata<- NULL
-        for(i in 1:ncol(data)){
-          data[sapply(data[,i],is.null),i]<- NA
-          ndata<- cbind(ndata, unlist(data[,i]))
-        }
-        ndata<- as.data.frame(ndata)
-        colnames(ndata)<- var.names
-      }
-      return(list(validation=validation,data =ndata))
-    }
-  }
-}
-
 # *******************************************************************************
 # Wrangling functions
 # *******************************************************************************
@@ -129,10 +75,6 @@ clean_prodcom <- function(df) {
     filter(Variable != c("SIC Totals and Non Production Headings"))
     
 }
-
-# Add leading 0s to unu_key column up to 4 digits to help match to other data
-# BoM_BEIS$UNU <- str_pad(BoM_BEIS$UNU, 4, pad = "0")
-
 
 #Get first date in the time series
 first_date <- function(x){
@@ -168,67 +110,4 @@ addUnits <- function(n)
                                  ifelse(n < 1e12, paste0(round(n/1e6), 'M'), # in billions
                                  ))))
   return(labels)
-}
-
-
-# *******************************************************************************
-# Renaming functions
-# *******************************************************************************
-
-# Import user-friendly names for codes
-UNU_colloquial <- read_xlsx( 
-  "./classifications/classifications/UNU_colloquial.xlsx") %>%
-  dplyr::rename(product = unu_description)
-
-# *******************************************************************************
-# statistical functions
-# *******************************************************************************
-
-# *******************************************************************************
-# Lifespans
-
-# Calculate CDF from Weibull parameters
-cdweibull <- function(x, shape, scale, log = FALSE){
-  dd <- dweibull(x, shape= shape, scale = scale, log = log)
-  dd <- 1-(cumsum(dd) * c(0, diff(x)))
-  return(dd)
-}
-
-# From Weibull par inverse mixdist
-weibullparinv <- function(shape, scale, loc = 0) 
-{
-  nu <- 1/shape
-  if (nu < 1e-6) {
-    mu <- scale * (1 + nu * digamma(1) + nu^2 * (digamma(1)^2 + 
-                                                   trigamma(1))/2)
-    sigma <- scale^2 * nu^2 * trigamma(1)
-  }
-  else {
-    mu <- loc + gamma(1 + (nu)) * scale
-    sigma <- sqrt(gamma(1 + 2 * nu) - (gamma(1 + nu))^2) * 
-      scale
-  }
-  data.frame(mu, sigma, loc)
-}
-
-# *******************************************************************************
-# Backcasting
-
-# Function to reverse time
-reverse_ts <- function(y)
-{
-  ts(rev(y), start=tsp(y)[1L], frequency=frequency(y))
-}
-
-# Function to reverse a forecast
-reverse_forecast <- function(object)
-{
-  h <- length(object[["mean"]])
-  f <- frequency(object[["mean"]])
-  object[["x"]] <- reverse_ts(object[["x"]])
-  object[["mean"]] <- ts(rev(object[["mean"]]),
-                         end=tsp(object[["x"]])[1L]-1/f, frequency=f)
-  object[["lower"]] <- object[["lower"]][h:1L,]
-  object[["upper"]] <- object[["upper"]][h:1L,]
-  return(object)
 }
