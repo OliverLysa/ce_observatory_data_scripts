cdfb21dbfceef08295ad7987526e170b285b7e92
OliverLysa <70466870+OliverLysa@users.noreply.github.com>
2025-03-26 08:44:48 -0600

    Update 03_collection_eol.R

--- plastics/baseline_model/03_collection_eol.R
+++ plastics/baseline_model/03_collection_eol.R
@@ -277,6 +277,45 @@ domestic_recycling_polymers <- read_xlsx("./cleaned_data/NPWD_recycling_recovery
 #######################
 # FLOW GROUP 7
 
+rejects <- read_ods("./raw_data/LA_collection.ods",
+                    sheet = "Table_1") %>%
+  row_to_names(3) %>%
+  select(1,2,5,6,23) %>%
+  clean_names() %>%
+  filter(authority_type != "Collection") %>%
+  mutate_at(c(5), as.numeric) %>%
+  group_by(financial_year) %>%
+  summarise(rejects = sum(local_authority_collected_estimated_rejects_tonnes)) 
+
+DBI::dbWriteTable(con,
+                  "LACW_rejects",
+                  rejects,
+                  overwrite = TRUE)
+
+# Calculate rejects as a percentage
+collection_flows_LA <- read_ods("./raw_data/LA_collection.ods",
+                                sheet = "Table_1") %>%
+  row_to_names(3) %>%
+  select(1,2,5,6,21:23) %>%
+  clean_names() %>%
+  filter(authority_type != "Collection") %>%
+  mutate_at(c(5:7), as.numeric) %>%
+  group_by(financial_year,region) %>%
+  summarise(recycling = sum(local_authority_collected_waste_sent_for_recycling_composting_reuse_tonnes),
+            residual = sum(local_authority_collected_waste_not_sent_for_recycling_tonnes),
+            rejects = sum(local_authority_collected_estimated_rejects_tonnes)) %>%
+  select(-rejects) %>%
+  pivot_longer(-c(financial_year,region),
+               names_to = "collection_route",
+               values_to = "tonnages") %>%
+  filter(collection_route == "recycling") %>%
+  group_by(financial_year) %>%
+  summarise(value = sum(tonnages))
+  
+rejects_share <- left_join(collection_flows_LA,
+                           rejects) %>%
+  mutate(share = rejects/value)
+
 ## Local Authority Collected Waste Estimated Rejects
 ## Rejects shared variable derived from running LACW_rejects.R in the data extraction folder
 rejects_WDF <- rejects_share %>%
